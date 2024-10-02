ccae011d3c5bca32d93c65c636c6c831b6e63d0c
Darshan Nair <darshannair26@gmail.com>
2024-10-02 18:45:28 -0400

    added image

--- animation/lib/main.dart
+++ animation/lib/main.dart
@@ -31,21 +31,35 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Fading Text Animation'),
+        title: const Text('Fading Text & Image Animation'),
       ),
       body: Center(
-        child: GestureDetector(
-          // Detect tap
-          onTap: toggleVisibility,
-          child: AnimatedOpacity(
-            opacity: _isVisible ? 1.0 : 0.0,
-            duration: const Duration(seconds: 1),
-            curve: Curves.easeInOut,
-            child: const Text(
-              'Hello, Flutter!',
-              style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: toggleVisibility,
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeInOut,
+              child: Image.network(
+                'https://png.pngtree.com/png-vector/20240127/ourmid/pngtree-halloween-cute-ghost-png-image_11555893.png',
+                width: 100,
+              ),
+            ),
+          ],
         ),
       ),
     );
